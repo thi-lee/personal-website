92bc966beeb8ac979801625da888b9ac6769f0ac
Thi Le <thilehy7865@gmail.com>
2021-07-01 17:02:57 +0700

    Getting started with react app - building portfolio

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Intro from "./components/Intro";
+
+function App() {
+  return <Intro />
+}
+
+export default App;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Intro() {
+    const name = "Thi Le";
+    let customStyle = {
+        top: ""
+    }
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>This is {name}</p>
+        </div>
+    )
+}
+
+export default Intro;
