0385132f2b1ca3821c866e534fba230c9a73f45f
Thi Le <thilehy7865@gmail.com>
2021-07-27 22:31:43 +0700

    bug on deployment

--- src/routes/P.jsx
+++ src/routes/P.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { TextHeading, Img } from "../components/letters/DisplayComps";
+import { TextHeading } from "../components/letters/DisplayComps";
+
+// import Slide from '@material-ui/core/Slide';
 
 function P() {
     return (
         <div className="display" id={"display"}>
             <TextHeading heading="Projects" />
-            {/* <Img src="./img/projects/project-restaurant-login.png" /> */}
+            
+            {/* <Slide> */}
+            {/* <Img src="./img/projects/project-restaurant-login.png" />
+            <Img src="./img/projects/project-restaurant-login.png" /> */}
+            {/* </Slide> */}
         </div>
         
     )
